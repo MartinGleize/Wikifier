f04dffb18918e16bb308165625a161e04bef6cdb
Dergeick <zenithmart1@hotmail.com>
2015-10-14 10:02:41 +0200

    First functional version, simply opens a new tab on the selected wiki term, even if it does not exist.

--- event_page.js
+++ event_page.js
@@ -1,13 +1,40 @@
 // License terms for this source code can be found in LICENSE 
 
-// the onClicked callback function
+function stringToUrlFormat(s) {
+    // replace spaces with underscores
+	var withoutSpace = s.replace(new RegExp(" ", 'g'), "_");
+    // encode the rest of the special characters
+    var res = encodeURI(withoutSpace);
+    return res;
+}
+
+function resolvedWikiUrl(baseUrl, term) {
+    var cuttingPoint = baseUrl.lastIndexOf("/");
+    var wikiRoot = baseUrl.substring(0, cuttingPoint);
+    var res = wikiRoot + "/" + term;
+    return res;
+}
+
+function navigateToTentativeWikiPage(url) {
+    chrome.tabs.create({"url": url});
+}
+
+// the onClicked callback function:
 function onClickHandler(info, tab) {
-  if (info.menuItemId == "wikify") {
-	console.log("The Wikifier context menu item was clicked.");
-	// The goal here is to look up the selected text:
-	// first directly in the wiki, as a page title
-	// second, as a search in the wiki to find closely related pages
-  }
+    if (info.menuItemId == "wikify") {
+        // here we look up the selected text on the current wiki, opens a new tab with the result
+        var text = info.selectionText;
+        var url = info.pageUrl;
+        console.log("The Wikifier context menu item was clicked.");
+        console.log("Selected text: " + text);
+        console.log("Page url: " + url);
+        // encoded wiki term
+        var term = stringToUrlFormat(text);
+        var newUrl = resolvedWikiUrl(url, term);
+        console.log("New url: " + newUrl);
+        // navigate to tentative wiki page (or search for it)
+        navigateToTentativeWikiPage(newUrl);
+	}
 };
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
